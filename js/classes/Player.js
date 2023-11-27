0552b5cb3db4707e600306e6264f4b77dcb46bbd
natansalvadorligabo <salvadorligabo@gmail.com>
2023-11-27 17:27:48 -0300

    adiciona tratamento de erros com try-catch e classe PlayerError

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -1,3 +1,10 @@
+class PlayerError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = 'PlayerError';
+    }
+}
+
 class Player extends Sprite {
     constructor({ collisionBlocks = [], imageSrc, frameRate }) {
         super({ imageSrc, frameRate })
@@ -23,106 +30,127 @@ class Player extends Sprite {
     }
 
     update() {
-        // hitbox - caixa azul
-        // c.fillStyle = 'rgba(0, 0, 255, 0.5)'
-        // c.fillRect(this.position.x, this.position.y, this.width, this.height)
-
-        // posicões em x e y mudam a partir das velocidades
-        this.position.x += this.velocity.x
-
-        // atualizando a hitbox
-        this.updateHitbox()
-
-        // checando se tem colisao horizontal
-        this.checkForHorizontalCollisions()
-
-        // aplicando gravidade
-        this.applyGravity()
-
-        // atualizando a hitbox
-        this.updateHitbox()
-
-        // c.fillRect(
-        //     this.hitbox.position.x,
-        //     this.hitbox.position.y,
-        //     this.hitbox.width,
-        //     this.hitbox.height)
-
-        // checando se tem colisao vertical
-        this.checkForVerticalCollisions()
-
-        /* bloco onde era aplicada a gravidade 
-        e checava colisao do chão do canvas nao tem mais necessidade */
+        try {
+            // hitbox - caixa azul
+            // c.fillStyle = 'rgba(0, 0, 255, 0.5)'
+            // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    
+            // posicões em x e y mudam a partir das velocidades
+            this.position.x += this.velocity.x
+    
+            // atualizando a hitbox
+            this.updateHitbox()
+    
+            // checando se tem colisao horizontal
+            this.checkForHorizontalCollisions()
+    
+            // aplicando gravidade
+            this.applyGravity()
+    
+            // atualizando a hitbox
+            this.updateHitbox()
+    
+            // c.fillRect(
+            //     this.hitbox.position.x,
+            //     this.hitbox.position.y,
+            //     this.hitbox.width,
+            //     this.hitbox.height)
+    
+            // checando se tem colisao vertical
+            this.checkForVerticalCollisions()
+    
+            /* bloco onde era aplicada a gravidade 
+            e checava colisao do chão do canvas nao tem mais necessidade */
+            
+        } catch (error) {
+            console.error("Ocorreu um erro durante a atualização do jogador: ", error);
+        }
     }
 
     updateHitbox() {
-        this.hitbox = {
-            position: {
-                x: this.position.x + 58,
-                y: this.position.y + 34,
-            },
-            width: 50,
-            height: 53,
+        try {
+            this.hitbox = {
+                position: {
+                    x: this.position.x + 58,
+                    y: this.position.y + 34,
+                },
+                width: 50,
+                height: 53,
+            }
+        } catch (error) {
+            throw new PlayerError("Erro ao atualizar a hitbox")
         }
     }
 
     checkForHorizontalCollisions() {
-        for (let i = 0; i < this.collisionBlocks.length; i++) {
-            const collisionBlock = this.collisionBlocks[i];
-
-            // se a colisao existir
-            if (
-                this.hitbox.position.x <= collisionBlock.position.x + collisionBlock.width &&
-                this.hitbox.position.x + this.hitbox.width >= collisionBlock.position.x &&
-                this.hitbox.position.y + this.hitbox.height >= collisionBlock.position.y &&
-                this.hitbox.position.y <= collisionBlock.position.y + collisionBlock.height
-            ) {
-                // se a colisao no eixo x for enquanto estiver indo para a esquerda
-                if (this.velocity.x < -0) {
-                    const offset = this.hitbox.position.x - this.position.x
-                    this.position.x = collisionBlock.position.x + collisionBlock.width - offset + 0.01;
-                    break;
-                }
-                // se a colisao no eixo x for enquanto estiver indo para a direita
-                if (this.velocity.x > 0) {
-                    const offset = this.hitbox.position.x - this.position.x + this.hitbox.width
-                    this.position.x = collisionBlock.position.x - offset - 0.01;
-                    break;
+        try {
+            for (let i = 0; i < this.collisionBlocks.length; i++) {
+                const collisionBlock = this.collisionBlocks[i];
+    
+                // se a colisao existir
+                if (
+                    this.hitbox.position.x <= collisionBlock.position.x + collisionBlock.width &&
+                    this.hitbox.position.x + this.hitbox.width >= collisionBlock.position.x &&
+                    this.hitbox.position.y + this.hitbox.height >= collisionBlock.position.y &&
+                    this.hitbox.position.y <= collisionBlock.position.y + collisionBlock.height
+                ) {
+                    // se a colisao no eixo x for enquanto estiver indo para a esquerda
+                    if (this.velocity.x < -0) {
+                        const offset = this.hitbox.position.x - this.position.x
+                        this.position.x = collisionBlock.position.x + collisionBlock.width - offset + 0.01;
+                        break;
+                    }
+                    // se a colisao no eixo x for enquanto estiver indo para a direita
+                    if (this.velocity.x > 0) {
+                        const offset = this.hitbox.position.x - this.position.x + this.hitbox.width
+                        this.position.x = collisionBlock.position.x - offset - 0.01;
+                        break;
+                    }
                 }
             }
+        } catch (error) {
+            throw new PlayerError("Erro ao verificar colisões horizontais")
         }
     }
 
     applyGravity() {
-        this.velocity.y += this.gravity;
-        this.position.y += this.velocity.y;
+        try {
+            this.velocity.y += this.gravity;
+            this.position.y += this.velocity.y;
+        } catch (error) {
+            throw new PlayerError("Erro ao aplicar gravidade")
+        }
     }
 
     checkForVerticalCollisions() {
-        for (let i = 0; i < this.collisionBlocks.length; i++) {
-            const collisionBlock = this.collisionBlocks[i];
-
-            // se a colisao existir
-            if (
-                this.hitbox.position.x + this.hitbox.width >= collisionBlock.position.x &&
-                this.hitbox.position.x <= collisionBlock.position.x + collisionBlock.width &&
-                this.hitbox.position.y + this.hitbox.height >= collisionBlock.position.y &&
-                this.hitbox.position.y <= collisionBlock.position.y + collisionBlock.height
-            ) {
-                // se a colisao no eixo y for enquanto estiver indo para cima
-                if (this.velocity.y < 0) {
-                    this.velocity.y = 0;
-                    this.position.y = collisionBlock.position.y + collisionBlock.height + 0.01;
-                    break;
-                }
-                // se a colisao no eixo y for enquanto estiver indo para baixo
-                if (this.velocity.y > 0) {
-                    this.velocity.y = 0;
-                    const offset = this.hitbox.position.y - this.position.y + this.hitbox.height
-                    this.position.y = collisionBlock.position.y - offset - 0.01;
-                    break;
+        try {
+            for (let i = 0; i < this.collisionBlocks.length; i++) {
+                const collisionBlock = this.collisionBlocks[i];
+    
+                // se a colisao existir
+                if (
+                    this.hitbox.position.x + this.hitbox.width >= collisionBlock.position.x &&
+                    this.hitbox.position.x <= collisionBlock.position.x + collisionBlock.width &&
+                    this.hitbox.position.y + this.hitbox.height >= collisionBlock.position.y &&
+                    this.hitbox.position.y <= collisionBlock.position.y + collisionBlock.height
+                ) {
+                    // se a colisao no eixo y for enquanto estiver indo para cima
+                    if (this.velocity.y < 0) {
+                        this.velocity.y = 0;
+                        this.position.y = collisionBlock.position.y + collisionBlock.height + 0.01;
+                        break;
+                    }
+                    // se a colisao no eixo y for enquanto estiver indo para baixo
+                    if (this.velocity.y > 0) {
+                        this.velocity.y = 0;
+                        const offset = this.hitbox.position.y - this.position.y + this.hitbox.height
+                        this.position.y = collisionBlock.position.y - offset - 0.01;
+                        break;
+                    }
                 }
             }
+        } catch (error) {
+            throw new PlayerError("Erro ao verificar colisões verticais")
         }
     }
 }
